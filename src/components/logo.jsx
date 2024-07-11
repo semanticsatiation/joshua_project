2cb9a7270fb8c42bcd248bd24b2a1b979152581e
Benjamin Candelario <74139058+semanticsatiation@users.noreply.github.com>
2024-07-11 12:55:39 -0400

    fix local image paths

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -3,7 +3,7 @@ import { getCloudFrontUrl } from '../utils/imageHelper';
 function LogoIntro() {
   return (
     <div id='logo-intro'>
-        <object id='logo' type="image/svg+xml" data={"dist/images/logo.svg"}></object>
+        <object id='logo' type="image/svg+xml" data={"public/images/logo.svg"}></object>
     </div>
   );
 }
